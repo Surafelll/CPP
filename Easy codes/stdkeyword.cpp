50df9bde6bc984d06a67f9b414723509a8ca52c6
Suraphel <surafelwondimagegn01@gmail.com>
2022-08-20 00:48:49 -0800

    string is not working without std

--- Easy codes/stdkeyword.cpp
+++ Easy codes/stdkeyword.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
-   std::cout << "" << std::endl;
+   std::cout << "endl is working with std" << std::endl;
    //output will new empty line
    std::cout << "Hello world";
    //Hello world will display
+   
+   string name="surafel";
+   
    return 0;
 }
