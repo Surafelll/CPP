200ffd2abd9f537ff0f1ea7265b032e2ea758db5
Suraphel <surafelwondimagegn01@gmail.com>
2022-08-24 02:32:08 -0800

    double type

--- Easy codes/typeofvarialble.cpp
+++ Easy codes/typeofvarialble.cpp
@@ -5,9 +5,12 @@ int main()
 {
     
     //type variable_name = value;
+    
     int x=3;
     //int is type,x is variable,= is assign and 3 is value
     cout<<"The value you stored in variable is : "<<x;
+
+    double y=3.14;
 
 
 
