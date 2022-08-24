05a5fa092bb14bb73b28b12672c5dbdd88916cbe
Suraphel <surafelwondimagegn01@gmail.com>
2022-08-24 02:36:17 -0800

    char type

--- Easy codes/typeofvarialble.cpp
+++ Easy codes/typeofvarialble.cpp
@@ -6,10 +6,12 @@ int main()
 
     // type variable_name = value;
 
-    int x = 3;
-    // int is type,x is variable,= is assign and 3 is value
+    int x = 3; // int is type,x is variable,= is assign and 3 is value
     cout << "The value you stored in variable x is : " << x << endl;
 
     double y = 3.14;
-    cout << "The value of you give for variable y is :" << y;
+    cout << "The value of you give for variable y is :" << y << endl;
+
+    char z = 'x';
+    cout << "your special character on variable is : " << z;
 }
