3e2e6b75b8603230362e703004f1a9d1c61d1a39
Suraphel <surafelwondimagegn01@gmail.com>
2022-08-24 02:34:52 -0800

    done working for double

--- Easy codes/typeofvarialble.cpp
+++ Easy codes/typeofvarialble.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 
-using namespace  std;
+using namespace std;
 int main()
 {
-    
-    //type variable_name = value;
-    
-    int x=3;
-    //int is type,x is variable,= is assign and 3 is value
-    cout<<"The value you stored in variable is : "<<x;
 
-    double y=3.14;
-    cout<<"The value of you give for variable is :" << y;
+    // type variable_name = value;
 
+    int x = 3;
+    // int is type,x is variable,= is assign and 3 is value
+    cout << "The value you stored in variable x is : " << x << endl;
 
-
+    double y = 3.14;
+    cout << "The value of you give for variable y is :" << y;
 }
