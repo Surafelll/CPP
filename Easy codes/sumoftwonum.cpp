10c58ca6c31ccd9ff1f6979cf286e1708ad0d598
Suraphel <surafelwondimagegn01@gmail.com>
2022-12-03 23:15:01 -0900

    done on sum

--- Easy codes/sumoftwonum.cpp
+++ Easy codes/sumoftwonum.cpp
@@ -4,9 +4,10 @@ int main()
 {
     cout << "This program is working on sum of two number";
     int num1, num2, sum;
-    cout << "first number";
+    cout << "\n first number";
     cin >> num1;
     cout << "second number";
     cin >> num2;
     sum = num1 + num2;
+    cout << sum;
 }
