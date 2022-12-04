4d029767017f76b045f06b549a01a5e4f6b45f47
Suraphel <surafelwondimagegn01@gmail.com>
2022-12-03 23:13:24 -0900

    sum statment is added

--- Easy codes/sumoftwonum.cpp
+++ Easy codes/sumoftwonum.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-        cout<<"This program is working on sum of two number";
-        int num1,num2,sum;
-        cout<<"first number";
-        cin>>num1;
-        cout<<"second number";
-        cin>>num2;
-
+    cout << "This program is working on sum of two number";
+    int num1, num2, sum;
+    cout << "first number";
+    cin >> num1;
+    cout << "second number";
+    cin >> num2;
+    sum = num1 + num2;
 }
