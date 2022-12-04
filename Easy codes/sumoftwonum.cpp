dfc772ce675ad985693df97a401b25ee408f310b
Suraphel <surafelwondimagegn01@gmail.com>
2022-12-03 23:12:47 -0900

    declare

--- Easy codes/sumoftwonum.cpp
+++ Easy codes/sumoftwonum.cpp
@@ -4,4 +4,9 @@ int main()
 {
         cout<<"This program is working on sum of two number";
         int num1,num2,sum;
+        cout<<"first number";
+        cin>>num1;
+        cout<<"second number";
+        cin>>num2;
+
 }
