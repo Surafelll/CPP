49ddb3e97949d39c720da031042d2382949438ac
Suraphel <surafelwondimagegn01@gmail.com>
2023-08-09 01:07:19 -0700

    strug

--- Easy codes/lib.cpp
+++ Easy codes/lib.cpp
@@ -4,9 +4,25 @@
 using namespace std;
 
 int main(){
-    string x;
+    bool x;
     cin>>x;
     cout<<endl<<x;
+    if (x=1)
+    {
+        cout<<endl<<"hi";
+
+        };
+
+    if (x=2);
+    {
+       cout<<endl<<"bye";
+        }
+
+
+    
+
+    
     return 0;
+    
     
 }
