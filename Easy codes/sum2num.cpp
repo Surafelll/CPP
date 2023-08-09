085b13359379a7598776e2e1c8e3e3aa6b49929f
Suraphel <surafelwondimagegn01@gmail.com>
2023-08-09 10:37:16 -0700

    cdfg

---
+++ Easy codes/sum2num.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    std::string name="hi";
+    std::cout<<"hello world";
+    std::cout<<name;
+
+    //return 0;
+}
