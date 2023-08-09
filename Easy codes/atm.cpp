f5dacd13e535f45332a591c975aa0069bb2445f7
Suraphel <surafelwondimagegn01@gmail.com>
2023-08-09 06:03:50 -0700

    nun

---
+++ Easy codes/atm.cpp
@@ -0,0 +1,27 @@
+                            // ***ATM SYSTEM***
+
+#include    <iostream>
+#include    <math.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"\t\t\t\t\tWelcome To Our Atm Service"<<endl;
+    cout<<"\t\t\tenter your 4 digit atm password";
+    int pass=1234;
+    cin>>pass;
+    if ( pass=1234)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        return 2;
+    }
+    
+    
+
+}
+
+
