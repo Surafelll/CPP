44475aff3d24e4a2bedfdf0d46030b0e194e28ee
Suraphel <surafelwondimagegn01@gmail.com>
2023-08-10 00:49:08 -0700

    njun

--- Easy codes/sum2num.cpp
+++ Easy codes/sum2num.cpp
@@ -4,9 +4,11 @@
 
 int main(int argc, char const *argv[])
 {
-    std::string name="hi";
-    std::cout<<"hello world";
-    std::cout<<name;
-
+    // std::string name="hi";
+    // std::cout<<"hello world";
+    // std::cout<<name;
+    char b;
+    b=b+0;
+    std::cout<<b;
     //return 0;
 }
