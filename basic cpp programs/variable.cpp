69342c0eb58f61d579b432b5db2a505a0461900e
Suraphel <surafelwondimagegn01@gmail.com>
2023-08-20 01:10:46 -0700

    done with double with

--- basic cpp programs/variable.cpp
+++ basic cpp programs/variable.cpp
@@ -5,10 +5,16 @@ using namespace std;
 int main()
 {
 
-int x;
-x=5;
-cout<<"|The number stored on x is : " <<x;
+    int x;
+    x = 5;
+    cout << "\n\t | The number stored on x is : " << x;
+
+    double y;
+    // float variable declaration and initialization.
+    y = 10.234f; // floating point data type
+    cout << "\n\t | The floating value of y is: " << y;
+
+
 
     return 0;
-
 }
