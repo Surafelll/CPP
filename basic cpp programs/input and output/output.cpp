1a07b81f6630d919cd320941b8305e8e7e2e55af
Suraphel <surafelwondimagegn01@gmail.com>
2023-08-22 00:39:35 -0700

    cerr

--- basic cpp programs/input and output/output.cpp
+++ basic cpp programs/input and output/output.cpp
@@ -5,6 +5,10 @@ main()
 
 {
     int a=11;
-    cout<<"The number on variable is a : "<<a;
+    cout<<"The number on variable is a : "<<a<<endl;
+
+    cerr<<"Message to display error"<<endl;
+    
+    
 } 
 
