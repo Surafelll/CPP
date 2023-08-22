bb9b9aca1061906f93c0ec87461df1a036666b8e
Suraphel <surafelwondimagegn01@gmail.com>
2023-08-21 23:40:09 -0700

    struggle with char16_t

---
+++ basic cpp programs/variable/char.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdint>
+
+
+
+int main()
+
+{
+    char16_t c= u'\u0042';
+    char x;
+    x='s'; 
+    std::cout<<x<<std::endl;
+
+
+  
+  std:: cout<<c;
+   return 0;
+}
