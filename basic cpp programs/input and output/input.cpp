2ad48e89931b4d03c0cacee10a2b16a20d0a3c87
Suraphel <surafelwondimagegn01@gmail.com>
2023-08-22 00:37:38 -0700

    ...

--- basic cpp programs/input and output/input.cpp
+++ basic cpp programs/input and output/input.cpp
@@ -3,10 +3,10 @@ using namespace std;
 main()
 
 {
-   int x;
-   cout<<"enter any number"<<endl;
+    int x;
+    cout << "enter any number" << endl;
 
-   cin>>x;
-   cout<<endl;
-   cout<<"The number you enter is : "<<x;
+    cin >> x;
+    cout << endl;
+    cout << "The number you enter is : " << x;
 }
